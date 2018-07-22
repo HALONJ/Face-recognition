d0d6767231d3dc6252d743a9490836362d2c7e95
Neeraj Patel <33634164+neeraj786patel@users.noreply.github.com>
2018-07-22 23:11:44 +0530

    Add files via upload

---
+++ sam.py
@@ -0,0 +1,7 @@
+import cv2, os
+import numpy as np
+from PIL import Image
+
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
